20b4d5575f08a1f95bd79371f0bccb9ef4a23799
Matyushkov Andrey <anmatyushkov@yandex.ru>
2023-04-21 19:03:43 +0300

    функция main

---
+++ Logger/main.cpp
@@ -0,0 +1,18 @@
+﻿#define __CRTDBG_MAP_ALLOC
+#include <crtdbg.h>
+#define DEBUG_NEW new(_NORMAL_BLOCK, __FILE__, __LINE__)
+#define new DEBUG_NEW
+#include "logger.h"
+
+int main() {
+	Logger* one = new FileLogger("log.txt", DTLI), *two = new ConsoleLogger(STLI);
+	one->log("helo");
+	two->log("asdas");
+	one->log("helo");
+	two->log("asdas");
+
+	delete one, two;
+
+	_CrtDumpMemoryLeaks();
+	return 0;
+}
